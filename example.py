d0618ee685c95ec5b7ccafbaea8fa18f1129e3f8
Bubbr <37887623+Bubbr@users.noreply.github.com>
2020-04-28 18:25:59 -0500

    Add files via upload

--- example.py
+++ example.py
@@ -1,14 +1,42 @@
+import gdtool as gdt
 from gdtool import Level
+import os
 
-if __name__ == "__main__":
+def main():
     print(
         '\n\tBienvenido a GDT (Geometry Dash Tool) by Felis\n'
         'Documentación y codigo fuente disponible en https://github.com/Bubbr/gd-tool\n'
         )
-    lvl = Level(byName="Bloodbath")
-    
+    lvl = Level(byName=str(input("Nombre del nivel\n> ")))
     print(
+        '\nDatos del nivel\n'
         f'\n\tNombre: {lvl.name}\n'
         f'\tId: {lvl.id}\n'
         f'\tCreador: {lvl.author.name}\n'
         )
+    print(
+        'Datos del creador\n\n'
+        f'\tNombre: {lvl.author.name}\n'
+        f'\tId: {lvl.author.id}\n'
+        f'\tSecret coins: {lvl.author.secretCoins}\n'
+        f'\tUser coins: {lvl.author.userCoins}\n'
+        f'\tStars: {lvl.author.stars}\n'
+        f'\tDiamonds: {lvl.author.diamonds}\n'
+        f'\tDemons: {lvl.author.demons}\n'
+        )
+
+if __name__ == "__main__":
+    print(f"secret: {gdt.secret}")
+
+    main()
+    while True:
+        op = int(input("¿Intentar de nuevo?\n\t[1] Sí\n\t[2] No\n> "))
+        if op == 1:
+            os.system('cls')
+            main()
+        else:
+            input('\n\tHasta la próxima! Gracias por usar GDT.\n'
+                  '\nPresione enter para salir...'
+                  )
+            break
+        
